4a2ea169096b2356fe95bc08017327b099be1b4e
takeda-shin <kgcfy958@yahoo.co.jp>
2020-03-15 22:49:58 +0900

    edit_top_page_design

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,62 +1,55 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="utf-8">
+  <meta charset="UTF-8">
+  <meta name="viewport" conyent="width=device-width, initial-scale=1.0, minimum-scale=1.0">
   <title>My Portfolio</title>
-  <link rel="stylesheet" href="/css/styles.css">
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href= "/css/normalize.css">
+  <link rel="stylesheet" href= "/css/home-style.css">
 </head>
 <body>
-
-  <div class="header">
-    @guest
-      <a href="{{ route('login') }}" class="header-back">Login</a>
-      @if (Route::has('register'))
-      <a href="{{ route('register') }}" class="header-back">register</a>
-      @endif
-    @else
-     <a href="{{ url('/posts/create') }}" class="header-back">Create</a>
-     <a href="{{ route('login') }}" class="header-back">Logout</a>
-     <!-- <a class="header-back" href="{{ route('login') }}"
-        onclick="event.preventDefault();
-                      document.getElementById('logout-form').submit();">
-        {{ __('Logout') }}
-    </a> -->
-    @endguest
-  </div>
-
-  <div class="container container-main">
-    <div class="container-main__title">
-      <h1>
-        My Portfolio
-      </h1>
-    </div>
-
-    <div class="container-main__subtitle">
-      <h2>
-        I am here. Next is...
-      </h2>
-    </div>
-
-    <div class="container-main__content">
-      <ul>
-        @if(count($post) > 0)
-          @foreach ($post as $post)
-          <li>
-            <a href="{{ action('PostsController@show', $post->id ) }}">{{ $post->title }}</a>
-            <a href= "#" class="del" data-id= "{{ $post->id }}" >[×]</a>
-            <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
-              {{ csrf_field() }}
-              {{ method_field('delete') }}
-            </form>
-          </li>
-          @endforeach
+  <header class="header">
+    <p class="site-title-sub">５秒で作れる自分専用のポートフォリオ</p>
+    <h1 class="site-title">MY Portfolio</h1>
+    <p class="site-description">I am here. Next is...</p>
+    <div class="buttons">
+      @guest
+        <a href="{{ route('login') }}" class="button">ログイン</a>
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}" class="button button-showy">新規登録</a>
         @endif
-      </ul>
-      <h3>5秒で作れる"自分専用"のポートフォリオサイト</h3>
+      @else
+        <a href="{{ url('/posts/create') }}" class="button">ポートフォリオを作る</a>
+        <a href="{{ route('logout') }}" class="button button-showy"
+          onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">
+            ログアウト
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+          @csrf
+        </form>
+      @endguest
     </div>
-
-
-
-    <script src="/js/main.js"></script>
+  </header>
+  <div class="container-main__content">
+    <ul>
+      @if(count($post) > 0)
+        @foreach ($post as $post)
+        <li>
+          <a href="{{ action('PostsController@show', $post->id ) }}">{{ $post->title }}</a>
+          <a href= "#" class="del" data-id= "{{ $post->id }}" >[×]</a>
+          <form method="post" action="{{ url('/posts', $post->id) }}" id="form_{{ $post->id }}">
+            {{ csrf_field() }}
+            {{ method_field('delete') }}
+          </form>
+        </li>
+        @endforeach
+      @endif
+    </ul>
+  </div>
+  <script src="/js/placeholders.min.js"></script>
+  <script src="/js/main.js"></script>
 </body>
 </html>
