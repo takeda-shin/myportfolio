b61254a2cf2efbdd9d25c6dbc77124b901d24715
takeda-shin <kgcfy958@yahoo.co.jp>
2020-03-26 22:06:15 +0900

    under 4 works

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -84,8 +84,7 @@
   @endisset
   @endforeach
 
-  @foreach ($post->works as $work)
-  @isset($work)
+  @isset($post->works)
   <section class="works">
     <h2 class="heading">WORKS</h2>
     @if ($works_count == 1)
@@ -99,6 +98,7 @@
     @elseif ($works_count >= 5)
     <div class="works-wrapper works-upper-four">
     @endif
+      @foreach ($post->works as $work)
       <div class="work-box">
         <img class="work-image" src="{{ asset('storage/image/' . $work->image) }}" alt="制作事例１">
         <div class="work-description">
@@ -113,10 +113,10 @@
           </div>
         </div>
       </div>
+      @endforeach
     </div>
   </section>
   @endisset
-  @endforeach
 
   <section class="contact" id="contact">
     <h2 class="heading">CONTACT</h2>
