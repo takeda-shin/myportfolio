cae0bc48a5c75595cb1170722fde316ab8365a9b
takeda-shin <kgcfy958@yahoo.co.jp>
2020-03-27 21:06:28 +0900

    redesign

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,55 +1,38 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="utf-8">
-  <title>New Portfolio</title>
-  <link rel="stylesheet" href="/css/new.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" conyent="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+  <title>My Portfolio</title>
+  <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href= "/css/normalize.css">
+  <link rel="stylesheet" href= "/css/create_portfolio.css">
 </head>
 <body>
-
-  <div class="header">
-    <a href="{{ url('/') }}" class="header-back">Back</a>
-  </div>
-
-  <div class="container container-main">
-    <div class="container-main__title">
-      <h1>New Portfolio</h1>
+  <header class="header">
+    <h1 class="site-title">Create Portfolio</h1>
+    <form method="POST" action= "{{ url('/posts') }}" class="contact-form php-mail-form">
+    {{ csrf_field() }}
+    <div class="form-group">
+      <input type="hidden" name="user_id" value="{{ $user_id }}">
+      <input type="text" name="title" placeholder="ポートフォリオのタイトル" data-rule="minlen:4">
+      <!-- エラーメッセージ -->
+      @if ($errors->has('title'))
+      <span class="error">{{ $errors->first('title') }}</span>
+      @endif
     </div>
-    <div class="container-main__message">
-      <h3>きみの存在を世界に示そう！</h3>
+
+    <div class="form-group">
+      <textarea name="body" id="contact-message" placeholder="ポートフォリオのキャッチコピー" rows="5"></textarea>
+      <!-- エラーメッセージ -->
+      @if ($errors->has('body'))
+      <span class="error">{{ $errors->first('body') }}</span>
+      @endif
     </div>
-
-    <div class="container__contact-form">
-      <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <form class="contact-form php-mail-form" method="post" action= "{{ url('/posts') }}">
-            {{ csrf_field() }}
-            <div class="form-group">
-              <input type="hidden" name="user_id" value="{{ $user_id }}">
-              <input type="" name="title" class="form-control" id="contact-name" placeholder="Title" data-rule="minlen:4">
-              <!-- エラーメッセージ -->
-              @if ($errors->has('title'))
-              <span class="error">{{ $errors->first('title') }}</span>
-              @endif
-            </div>
-
-            <div class="form-group">
-              <textarea class="form-control" name="body" id="contact-message" placeholder="Catch Copy" rows="5"></textarea>
-              <!-- エラーメッセージ -->
-              @if ($errors->has('body'))
-              <span class="error">{{ $errors->first('body') }}</span>
-              @endif
-            </div>
-
-            <div class="form-send">
-              <button type="submit" class="btn btn-large">Create</button>
-            </div>
-
-          </form>
-        </div>
-      </div>
-     </div>
-
-
+    <input type="submit" value="CREATE">
+  </header>
+  <script src="/js/placeholders.min.js"></script>
+  <script src="/js/main.js"></script>
 </body>
 </html>
